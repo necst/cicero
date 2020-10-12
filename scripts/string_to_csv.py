3ea90a02585c7c50665952b3194fef77e3bfb4a0
Daniele Parravicini <daniparra@hotmail.it>
2020-10-12 12:10:13 +0200

    Add scritps to simplify testbench execution

---
+++ scripts/string_to_csv.py
@@ -0,0 +1,6 @@
+filepath = 'string_ok.csv'
+string = input('input string>')
+byte_list = string.encode('ascii', 'replace')+ b'\0' 
+with open(filepath, 'w') as f:
+    for b in byte_list:
+        f.write(str(int(b))+'\n')
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
 #example time bash ./test.sh 'a*' aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-for n in {1..10000..1}
+for n in {1..1..1}
 do
     # grep -x -E -q \'$1\' $2 
-    echo \'$2\' | grep -x -E -q \'$1\'
- 
-
+    #grep -q quiet
+    #echo $n
+    #echo $2
+    #echo $1
+    echo $2 | grep --colour=auto -E  $1
 done
