169d59875233e2c5f71dee4c77e41cd47f4dec70
Daniele Parravicini <daniparra@hotmail.it>
2020-10-29 09:26:57 +0100

    Removed unused modules and update AXI_top_tb

--- rtl_src/basic_block.sv
+++ rtl_src/basic_block.sv
@@ -43,24 +43,23 @@ module basic_block #(
     localparam I_WIDTH = 16;
     localparam OFFSET_I= $clog2(MEMORY_WIDTH/I_WIDTH);
     localparam CPU_MEMORY_ADDR_WIDTH = MEMORY_ADDR_WIDTH+OFFSET_I;
-    //output latency is unused by basic block.
+
     wire [LATENCY_COUNT_WIDTH-1:0] output_pc_latency_unused;
     assign output_pc_latency_unused = output_pc_latency;
-    //sub signals of input_pc_and_current, output_pc_and_current
+
     logic [PC_WIDTH-1:0]        output_pc, input_pc;
     logic                       input_pc_is_directed_to_current, output_pc_is_directed_to_current;
-    //signals for regex_cpu
+
     logic regex_cpu_running                                ;
     logic regex_cpu_input_pc_ready,regex_cpu_input_pc_valid;
     localparam                      REGEX_CPU_FIFO_WIDTH_POWER_OF_2 = 2;
     logic [REGEX_CPU_FIFO_WIDTH_POWER_OF_2:0] regex_cpu_latency;
-    //storage part of the basic block
-    //cache wires
+
     wire                        regex_cpu_memory_ready      ;
     wire [CPU_MEMORY_ADDR_WIDTH-1:0]regex_cpu_memory_addr       ;
     wire [I_WIDTH-1     :0]     regex_cpu_memory_data       ;
     wire                        regex_cpu_memory_valid      ;
-    //FIFO even signal 
+
     logic                       fifo_even_data_in_ready     ;
     logic                       fifo_even_data_in_not_ready ;
     logic [PC_WIDTH-1:0]        fifo_even_data_in           ;
@@ -70,7 +69,7 @@ module basic_block #(
     logic                       fifo_even_data_out_valid    ;
     logic                       fifo_even_data_out_not_valid;
     logic [FIFO_COUNT_WIDTH-1:0]fifo_even_data_count        ;
-    //FIFO odd signal
+
     logic                       fifo_odd_data_in_ready      ;
     logic                       fifo_odd_data_in_not_ready  ;
     logic [PC_WIDTH-1:0]        fifo_odd_data_in            ;
@@ -80,7 +79,7 @@ module basic_block #(
     logic                       fifo_odd_data_out_valid     ;
     logic                       fifo_odd_data_out_not_valid ;
     logic [FIFO_COUNT_WIDTH-1:0]fifo_odd_data_count         ;
-    //FIFO cur_char signal
+
     logic                       fifo_cur_char_data_in_ready  ;
     logic [PC_WIDTH-1:0]        fifo_cur_char_data_in        ;
     logic                       fifo_cur_char_data_in_valid  ;
@@ -88,7 +87,7 @@ module basic_block #(
     logic [PC_WIDTH-1:0]        fifo_cur_char_data_out       ;
     logic                       fifo_cur_char_data_out_valid ;
     logic [FIFO_COUNT_WIDTH-1:0]fifo_cur_char_data_count     ;
-    //FIFO next_char signal
+
     logic                       fifo_next_char_data_in_ready ;
     logic [PC_WIDTH-1:0]        fifo_next_char_data_in       ;
     logic                       fifo_next_char_data_in_valid ;
@@ -97,49 +96,46 @@ module basic_block #(
     logic                       fifo_next_char_data_out_valid;
     logic [FIFO_COUNT_WIDTH-1:0]fifo_next_char_data_count    ;
 
-    //FIFO even instantiation 
+
     fifo #(
         .DWIDTH(PC_WIDTH),
         .COUNT_WIDTH(FIFO_COUNT_WIDTH)
     )fifo_even(
         .clk         (clk                         ), 
         .reset       (reset                       ), 
-        .full        (fifo_even_data_in_not_ready ), //equivalent to not data_in_ready
+        .full        (fifo_even_data_in_not_ready ), 
         .din         (fifo_even_data_in           ),  
-        .wr_en       (fifo_even_data_in_valid     ), //equivalent to data_in_valid
-        .rd_en       (fifo_even_data_out_ready    ), //equivalent to data_out_ready
+        .wr_en       (fifo_even_data_in_valid     ), 
+        .rd_en       (fifo_even_data_out_ready    ), 
         .dout        (fifo_even_data_out          ), 
-        .empty       (fifo_even_data_out_not_valid), //equivalent to not data_out_valid
+        .empty       (fifo_even_data_out_not_valid), 
         .data_count  (fifo_even_data_count        )
     );
-    //conclusion fifo even instatiation convert negated not_ready/not_valid signals to "standard" ready/valid interface.
     assign fifo_even_data_in_ready  = ~ fifo_even_data_in_not_ready ;
     assign fifo_even_data_out_valid = ~ fifo_even_data_out_not_valid;
 
-    //FIFO odd instantiation 
+
     fifo #(
         .DWIDTH(PC_WIDTH),
         .COUNT_WIDTH(FIFO_COUNT_WIDTH)
     )fifo_odd(
         .clk         (clk                        ), 
         .reset       (reset                      ), 
-        .full        (fifo_odd_data_in_not_ready ), //equivalent to not data_in_ready
+        .full        (fifo_odd_data_in_not_ready ), 
         .din         (fifo_odd_data_in           ),  
-        .wr_en       (fifo_odd_data_in_valid     ), //equivalent to data_in_valid
-        .rd_en       (fifo_odd_data_out_ready    ), //equivalent to data_out_ready
+        .wr_en       (fifo_odd_data_in_valid     ), 
+        .rd_en       (fifo_odd_data_out_ready    ), 
         .dout        (fifo_odd_data_out          ), 
         .data_count  (fifo_odd_data_count        ),
-        .empty       (fifo_odd_data_out_not_valid) //equivalent to not data_out_valid
+        .empty       (fifo_odd_data_out_not_valid)
     );
-    //conclusion fifo odd instatiation convert negated not_ready/not_valid signals to "standard" ready/valid interface.
     assign fifo_odd_data_in_ready  = ~ fifo_odd_data_in_not_ready ;
     assign fifo_odd_data_out_valid = ~ fifo_odd_data_out_not_valid;
 
-    //select fifo current and fifo next according to cur_is_even_character signal
     always_comb begin : selector_fifo_current_fifo_next
         
         if ( cur_is_even_character == 1'b1  )
-        begin //even is current and odd is next_char
+        begin 
             fifo_cur_char_data_in_ready   =  fifo_even_data_in_ready      ;
             fifo_even_data_in             =  fifo_cur_char_data_in        ;
             fifo_even_data_in_valid       =  fifo_cur_char_data_in_valid  ; 
@@ -157,7 +153,7 @@ module basic_block #(
             fifo_next_char_data_count     =  fifo_odd_data_count          ;
         end
         else
-        begin //odd is current and even is next_char
+        begin 
             fifo_cur_char_data_in_ready   =  fifo_odd_data_in_ready       ;
             fifo_odd_data_in              =  fifo_cur_char_data_in        ;
             fifo_odd_data_in_valid        =  fifo_cur_char_data_in_valid  ; 
@@ -176,18 +172,14 @@ module basic_block #(
         end                        
     end
 
-    // make so that content of fifo_next_char is not consumed. 
+
     assign fifo_next_char_data_out_ready = 1'b0;
 
-    //output_pc is redirected toward output after having concatenated with output_pc_is_directed_to_current
     assign output_pc_and_current = {output_pc, output_pc_is_directed_to_current};
-    //input_pc_and_current is splitted in input_pc and input_pc_is_directed_to_current
+
     assign input_pc_is_directed_to_current = input_pc_and_current[0];
     assign input_pc                        = input_pc_and_current[1+:PC_WIDTH] ;
 
-    //demux to drive input_pc_and_current toward correct fifo.
-    //to avoid a combinational loop (switches can decide to move data toward one or the other output port depending on output ready, but in principle output ready depends also on targeted fifo which is specified in data). 
-    //conservative(certain instruction which in principle could have been stored are refused) but correct
     assign input_pc_ready         = fifo_cur_char_data_in_ready && fifo_next_char_data_in_ready;
     always_comb begin : demux_for_pc_in 
         fifo_cur_char_data_in  = { PC_WIDTH{1'b0} };
@@ -196,25 +188,21 @@ module basic_block #(
         if(input_pc_is_directed_to_current)
         begin
             fifo_cur_char_data_in       = input_pc ;
-            fifo_cur_char_data_in_valid = input_pc_valid && input_pc_ready; //since for outside bb input is ready if both fifo_cur_char and fifo_next_char are ready, their valid has to take into account that to avoid that fifo_latches it  
+            fifo_cur_char_data_in_valid = input_pc_valid && input_pc_ready;
 
             fifo_next_char_data_in_valid= 1'b0;
         end
         else
         begin
             fifo_next_char_data_in       = input_pc ;
-            fifo_next_char_data_in_valid = input_pc_valid && input_pc_ready; //since for outside bb input is ready if both fifo_cur_char and fifo_next_char are ready, their valid has to take into account that to avoid that fifo_latches it  
+            fifo_next_char_data_in_valid = input_pc_valid && input_pc_ready; 
 
             fifo_cur_char_data_in_valid  = 1'b0;
         end
     end
 
-    //compute the approximate latency seen outside thought of max between odd and even but lead to high fanout-> setup violation
-    //opted for a simpler computation: consider only fifo_cur_char length.
-    //always_comb begin : latency_computation
-    //    if( fifo_odd_data_count > fifo_even_data_count)  input_pc_latency = fifo_odd_data_count  + 1; 
-    //    else                                             input_pc_latency = fifo_even_data_count + 1;      
-    //end
+   
+
     if( CONSIDER_PIPELINE_FIFO == 1)
     begin
         always_comb
@@ -233,104 +221,63 @@ module basic_block #(
     end
 
     
-    //running if regex_cpu has taken some instruction and hence the data_out_ready=0
-    //        or some instructions are saved in curr character fifo and hence fifo_cur_char_data_out_valid=1
+
     always_comb begin : running_definition
         running = fifo_cur_char_data_out_valid || regex_cpu_running;
     end
 
 
-    /////////////////////////////////////////////////////////////////////////////
-    // Computing part of the basic block
-    /////////////////////////////////////////////////////////////////////////////
-    // go signal enable dequeue process from fifo_cur_char and plays the role of an enabler regex_cpu 
+
     assign fifo_cur_char_data_out_ready = go && regex_cpu_input_pc_ready    ; 
     assign regex_cpu_input_pc_valid     = go && fifo_cur_char_data_out_valid;
-    if(PIPELINED)
-    begin : g
+    
         
-        regex_cpu_pipelined #(
-            .PC_WIDTH                           (PC_WIDTH                           ),
-            .CHARACTER_WIDTH                    (CHARACTER_WIDTH                    ),
-            .MEMORY_WIDTH                       (I_WIDTH                       ),
-            .MEMORY_ADDR_WIDTH                  (CPU_MEMORY_ADDR_WIDTH              ),
-            .FIFO_WIDTH_POWER_OF_2              (REGEX_CPU_FIFO_WIDTH_POWER_OF_2    )
-        ) aregex_cpu (
-            .clk                                (clk                                ),
-            .reset                              (reset                              ), 
-            .current_character                  (current_character                  ),
-            .input_pc_ready                     (regex_cpu_input_pc_ready           ), 
-            .input_pc                           (fifo_cur_char_data_out             ), 
-            .input_pc_valid                     (regex_cpu_input_pc_valid           ),
-            .memory_ready                       (regex_cpu_memory_ready             ),
-            .memory_addr                        (regex_cpu_memory_addr              ),
-            .memory_data                        (regex_cpu_memory_data              ),   
-            .memory_valid                       (regex_cpu_memory_valid             ),
-            .output_pc_is_directed_to_current   (output_pc_is_directed_to_current   ),
-            .output_pc_ready                    (output_pc_ready                    ),
-            .output_pc                          (output_pc                          ),
-            .output_pc_valid                    (output_pc_valid                    ),
-            .accepts                            (accepts                            ),
-            .running                            (regex_cpu_running                  ),
-            .latency                            (regex_cpu_latency                  )
-        );
-    end
-    else
-    begin : g 
-        regex_cpu #(
-            .PC_WIDTH                           (PC_WIDTH                           ),
-            .CHARACTER_WIDTH                    (CHARACTER_WIDTH                    ),
-            .MEMORY_WIDTH                       (I_WIDTH                       ),
-            .MEMORY_ADDR_WIDTH                  (CPU_MEMORY_ADDR_WIDTH              )
-        ) aregex_cpu (
-            .clk                                (clk                                ),
-            .reset                              (reset                              ), 
-            .current_character                  (current_character                  ),
-            .input_pc_ready                     (regex_cpu_input_pc_ready           ), 
-            .input_pc                           (fifo_cur_char_data_out             ), 
-            .input_pc_valid                     (regex_cpu_input_pc_valid           ),
-            .memory_ready                       (regex_cpu_memory_ready             ),
-            .memory_addr                        (regex_cpu_memory_addr              ),
-            .memory_data                        (regex_cpu_memory_data              ),   
-            .memory_valid                       (regex_cpu_memory_valid             ),
-            .output_pc_is_directed_to_current   (output_pc_is_directed_to_current   ),
-            .output_pc_ready                    (output_pc_ready                    ),
-            .output_pc                          (output_pc                          ),
-            .output_pc_valid                    (output_pc_valid                    ),
-            .accepts                            (accepts                            )
-        );
-        assign regex_cpu_latency = 0;
-        assign regex_cpu_running =  ~fifo_cur_char_data_out_ready ;
-    end
+    regex_cpu_pipelined #(
+        .PC_WIDTH                           (PC_WIDTH                           ),
+        .CHARACTER_WIDTH                    (CHARACTER_WIDTH                    ),
+        .MEMORY_WIDTH                       (I_WIDTH                       ),
+        .MEMORY_ADDR_WIDTH                  (CPU_MEMORY_ADDR_WIDTH              ),
+        .FIFO_WIDTH_POWER_OF_2              (REGEX_CPU_FIFO_WIDTH_POWER_OF_2    )
+    ) aregex_cpu (
+        .clk                                (clk                                ),
+        .reset                              (reset                              ), 
+        .current_character                  (current_character                  ),
+        .input_pc_ready                     (regex_cpu_input_pc_ready           ), 
+        .input_pc                           (fifo_cur_char_data_out             ), 
+        .input_pc_valid                     (regex_cpu_input_pc_valid           ),
+        .memory_ready                       (regex_cpu_memory_ready             ),
+        .memory_addr                        (regex_cpu_memory_addr              ),
+        .memory_data                        (regex_cpu_memory_data              ),   
+        .memory_valid                       (regex_cpu_memory_valid             ),
+        .output_pc_is_directed_to_current   (output_pc_is_directed_to_current   ),
+        .output_pc_ready                    (output_pc_ready                    ),
+        .output_pc                          (output_pc                          ),
+        .output_pc_valid                    (output_pc_valid                    ),
+        .accepts                            (accepts                            ),
+        .running                            (regex_cpu_running                  ),
+        .latency                            (regex_cpu_latency                  )
+    );
 
-    //depending on CACHE_WIDTH_BITS
-    if (CACHE_WIDTH_BITS <= 0)
-    begin
-        assign memory_addr              = regex_cpu_memory_addr ;
-        assign memory_valid             = regex_cpu_memory_valid;  
-        assign regex_cpu_memory_ready   = memory_ready          ;
-        assign regex_cpu_memory_data    = memory_data           ;   
-    end
-    else
-    begin
-        cache_block_directly_mapped #(          
-            .DWIDTH             (I_WIDTH                ),
-            .CACHE_WIDTH_BITS   (CACHE_WIDTH_BITS       ),
-            .BLOCK_WIDTH_BITS   (CACHE_BLOCK_WIDTH_BITS ),
-            .ADDR_IN_WIDTH      (CPU_MEMORY_ADDR_WIDTH  )
-        ) a_cache (
-            .clk                (clk                    ),
-            .reset              (reset                  ),
-            .addr_in_valid      (regex_cpu_memory_valid ),
-            .addr_in            (regex_cpu_memory_addr  ),
-            .addr_in_ready      (regex_cpu_memory_ready ),
-            .data_out           (regex_cpu_memory_data  ),
-            .addr_out_valid     (memory_valid           ),
-            .addr_out           (memory_addr            ),
-            .addr_out_ready     (memory_ready           ),
-            .data_in            (memory_data            )
-        );
-    end
+
+ 
+    cache_block_directly_mapped #(          
+        .DWIDTH             (I_WIDTH                ),
+        .CACHE_WIDTH_BITS   (CACHE_WIDTH_BITS       ),
+        .BLOCK_WIDTH_BITS   (CACHE_BLOCK_WIDTH_BITS ),
+        .ADDR_IN_WIDTH      (CPU_MEMORY_ADDR_WIDTH  )
+    ) a_cache (
+        .clk                (clk                    ),
+        .reset              (reset                  ),
+        .addr_in_valid      (regex_cpu_memory_valid ),
+        .addr_in            (regex_cpu_memory_addr  ),
+        .addr_in_ready      (regex_cpu_memory_ready ),
+        .data_out           (regex_cpu_memory_data  ),
+        .addr_out_valid     (memory_valid           ),
+        .addr_out           (memory_addr            ),
+        .addr_out_ready     (memory_ready           ),
+        .data_in            (memory_data            )
+    );
+
 
     
 endmodule
--- sim/AXI_top_tb_from_file.sv
+++ sim/AXI_top_tb_from_file.sv
@@ -9,7 +9,6 @@ module AXI_top_tb_from_file();
     logic                             reset; 
     logic [REG_WIDTH-1:0]  data_in_register;
     logic [REG_WIDTH-1:0]  address_register;
-    //logic [REG_WIDTH-1:0]  start_pc_register;
     logic [REG_WIDTH-1:0]  start_cc_pointer_register;
     logic [REG_WIDTH-1:0]      cmd_register;
     logic [REG_WIDTH-1:0]   status_register;
@@ -20,14 +19,10 @@ module AXI_top_tb_from_file();
     .reset(                        reset),
     .data_in_register( data_in_register ),
     .address_register( address_register ),
-    //.start_pc_register( start_pc_register),
     .start_cc_pointer_register(start_cc_pointer_register),
     .cmd_register(     cmd_register     ),
     .status_register(  status_register  ),
     .data_o_register(  data_o_register  )
-
-    
-
     );
 
     // clock generator  
@@ -300,131 +295,7 @@ module AXI_top_tb_from_file();
     end
     endtask
 
-    localparam BB_N = 16;
-    int  bb_cc_active [BB_N-1:0] ;
-
-    task display_utilization(input logic [REG_WIDTH-1:0] cc_taken);
-    begin
-        int i;
-        real u;
-        $write("BB utilization ");
-        for (i = 0 ; i< BB_N ; i+=1) 
-        begin
-            u = $itor(bb_cc_active[i])/$itor(cc_taken);
-            $write("\t %f",u);
-        end
-        $write("\n");
-    end
-    endtask
-    localparam UTILIZATON_ENABLED = 1'b0;
-
-    genvar i;
-    generate
-
-        if(UTILIZATON_ENABLED)
-        begin
-            for(i = 0; i<BB_N; i+=1)begin
-                always @( posedge clk)
-                begin
-                   
-                    if (reset)
-                    begin
-                            bb_cc_active[i] <= 32'd0;
-                    end
-                    else if(dut.g1.a_regex_coprocessor.bbs_go == 1'b0)
-                    begin
-                            if(i==0)
-                            begin
-                                int j, tot;
-                                real u;
-                                tot=0;
-                                for (j = 0 ; j< BB_N ; j+=1) 
-                                begin
-                                    tot += (bb_cc_active[j]);
-                                end
-                                if(tot !== 0)begin
-                                    $write("BB utilization this iteration");
-                                    for (j = 0 ; j< BB_N ; j+=1) 
-                                    begin
-                                        u = $itor(bb_cc_active[j])/$itor(tot);
-                                        $write("\t %f",u);
-                                    end
-                                    $write("\n");
-                                    bb_cc_active[i] <= 32'd0;
-                                end
-                                
-                            end
-                            
-                    end
-                    else
-                    begin
-                        if(BB_N > 1)
-                        begin
-                            
-                            if( dut.g1.a_regex_coprocessor.g[i].abb.g.aregex_cpu.running)
-                            begin
-                                bb_cc_active[i] <= bb_cc_active[i]+1;
-                            end
-                        end
-                        //else
-                        //begin
-                        //    if( dut.g1.a_regex_coprocessor.g.abb.g.aregex_cpu.running)
-                        //    begin
-                        //        bb_cc_active[i] <= bb_cc_active[i]+1;
-                        //    end
-                        //end
-                    end
-                    
-                end
-            end
-        end
-    endgenerate 
-    /*always @( posedge clk)
-    begin
-        int i;
-        
-        if (reset)
-        begin
-            for (i = 0 ; i< BB_N ; i+=1) begin
-                bb_cc_active[i] <= 32'd0;
-            end
-        end
-        else 
-        begin
-            if(BB_N > 1)
-            begin
-                //for 1bb
-                //if( dut.g1.a_regex_coprocessor.g.abb.g.aregex_cpu.running)
-                //begin
-                //    bb_cc_active[0] <= bb_cc_active[0]+1;
-                //end
-                if( dut.g1.a_regex_coprocessor.g[0].abb.g.aregex_cpu.running)
-                begin
-                    bb_cc_active[0] <= bb_cc_active[0]+1;
-                end
-                if( dut.g1.a_regex_coprocessor.g[1].abb.g.aregex_cpu.running)
-                begin
-                    bb_cc_active[1] <= bb_cc_active[1]+1;
-                end
-                if( dut.g1.a_regex_coprocessor.g[2].abb.g.aregex_cpu.running)
-                begin
-                    bb_cc_active[2] <= bb_cc_active[2]+1;
-                end
-                if( dut.g1.a_regex_coprocessor.g[3].abb.g.aregex_cpu.running)
-                begin
-                    bb_cc_active[3] <= bb_cc_active[3]+1;
-                end
-            end
-            //else
-            //begin
-            //    if( dut.g1.a_regex_coprocessor.g.abb.g.aregex_cpu.running)
-            //    begin
-            //        bb_cc_active[i] <= bb_cc_active[i]+1;
-            //    end
-            //end
-        end
-        
-    end*/
+    
 
     initial
     begin
@@ -448,7 +319,7 @@ module AXI_top_tb_from_file();
             @(posedge clk);
         
         //1.write code
-        fp_code= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor\\sim\\a.out","r");
+        fp_code= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_pynq\\sim\\a(bORc)star.csv","r");
         if (fp_code==0)
         begin
             $display("Could not open file '%s' for reading","code.csv");
@@ -460,14 +331,14 @@ module AXI_top_tb_from_file();
         write_file(fp_code, start_code , end_code );
         
         //2, write string
-        fp_string= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor\\scripts\\string_ok.csv","r");
+        fp_string= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_pynq\\sim\\string_ok.csv","r");
         if (fp_string==0)
         begin
             $display("Could not open file '%s' for reading","string_ok.csv");
             $stop;     
         end
         
-        //when writing 32bits in a bram that support 16bit reading, address has to be aligned at 2 bytes.
+
         start_string = end_code;
         //write string
         $display("writing string from %h",start_string);
@@ -491,7 +362,6 @@ module AXI_top_tb_from_file();
         wait_result(res);
         get_cc_elapsed(cc_taken);
         $display("cc taken: %d", cc_taken);
-        display_utilization(cc_taken);
         if( res == 1)
         begin
             $display("OK: string accepted");
@@ -501,7 +371,7 @@ module AXI_top_tb_from_file();
             $display("NOK: string rejected");
             $stop(1);
         end 
-        $finish();
+        
         cmd_register<= CMD_RESET;
         @(posedge clk);
         cmd_register<= CMD_NOP;
@@ -520,7 +390,7 @@ module AXI_top_tb_from_file();
             @(posedge clk);
 
         //1.write code
-        fp_code= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor\\sim\\a(bORc)star.csv","r");
+        fp_code= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_pynq\\sim\\a(bORc)star.csv","r");
         if (fp_code==0)
         begin
             $display("Could not open file '%s' for reading","code.csv");
@@ -532,14 +402,13 @@ module AXI_top_tb_from_file();
         write_file(fp_code, start_code , end_code );
         
         //2, write string
-        fp_string= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor\\sim\\string_nok.csv","r");
+        fp_string= $fopen("C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_pynq\\sim\\string_nok.csv","r");
         if (fp_string==0)
         begin
             $display("Could not open file '%s' for reading","string_nok.csv");
             $stop;     
         end
-        
-        //when writing 32bits in a bram that support 16bit reading, address has to be aligned at 2 bytes.
+
         start_string = end_code;
         //write string
         $display("writing string from %h",start_string);
@@ -570,7 +439,6 @@ module AXI_top_tb_from_file();
         
         get_cc_elapsed(cc_taken);
         $display("cc taken: %d", cc_taken);
-        display_utilization(cc_taken);
 
         $finish(0);
     end
