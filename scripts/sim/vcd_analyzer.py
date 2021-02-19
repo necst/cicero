ea8289e9c2e951b163c90169dad2c441251571d7
Daniele Parravicini <daniparra@hotmail.it>
2021-02-19 13:01:05 +0100

    Break graphs into smaller graphs

--- scripts/sim/vcd_analyzer.py
+++ scripts/sim/vcd_analyzer.py
@@ -13,63 +13,64 @@ import matplotlib.pyplot as plt
 HIERARCHY_DELIMITER = '.'
 
 def lower_hierarchy(path, subPath):
-    
-    return path+HIERARCHY_DELIMITER+subPath
+	
+	return path+HIERARCHY_DELIMITER+subPath
 
 def raise_hierarchy(path, n=1):
-    while n > 0:
-        new_path_index = path.rfind(HIERARCHY_DELIMITER)
-        path = path[:new_path_index]
-        n -=1
-    return path
+	while n > 0:
+		new_path_index = path.rfind(HIERARCHY_DELIMITER)
+		path = path[:new_path_index]
+		n -=1
+	return path
 
 def getSignalsByRegex(vcd, reg_string):
-    regex = re.compile(reg_string)
-    return vcd[regex]
+	regex = re.compile(reg_string)
+	return vcd[regex]
 
 def getEngines(vcd):
-    reg    = re.compile(".*anEngine\.clk$")
-    return  vcd[reg]
+	reg    = re.compile(".*anEngine\.clk$")
+	return  vcd[reg]
 
 def getModule(vcd, module_name):
-    reg     = re.compile(f".*{module_name}$")
-    return  vcd[reg]
-    
+	reg     = re.compile(f".*{module_name}$")
+	return  vcd[reg]
+	
 
 def getClockPeriod(vcd):
-    clk_signal = getSignalsByRegex(vcd, ".*clk")[0]
-    clk_signal = vcd[clk_signal].tv
-    clk_period = (clk_signal[2][0] - clk_signal[1][0])*2
-    return clk_period
+	clk_signal = getSignalsByRegex(vcd, ".*clk")[0]
+	clk_signal = vcd[clk_signal].tv
+	clk_period = (clk_signal[2][0] - clk_signal[1][0])*2
+	return clk_period
 
 def remove_non_maximal_scopes(scopes):
-    i=0
-    j=0
-    while i +1 < len(scopes):
-        e = scopes[i]
-        j = i+1
-        while(j< len(scopes)):
-            if scopes[j].find(e) >= 0:
-                del scopes[j]
-            else:
-                j+=1
-        i+=1
-    return scopes
+	i=0
+	j=0
+	while i +1 < len(scopes):
+		e = scopes[i]
+		j = i+1
+		while(j< len(scopes)):
+			if scopes[j].find(e) >= 0:
+				del scopes[j]
+			else:
+				j+=1
+		i+=1
+	return scopes
 
 if len(sys.argv) > 1:
-    vcd_path = sys.argv[1]
+	vcd_path = sys.argv[1]
 else:
-    #vcd_path = "C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_safe\\proj\\regex_copro\\regex_copro.sim\\sim_1\\behav\\xsim\\test.vcd"
-    vcd_path = "test4_regex22_string1.vcd"
-    #vcd_path = "test_1.vcd"
-    #print('Give me a vcd file to parse')
-    #sys.exit(-1)
+	#vcd_path = "C:\\Users\\danie\\Documents\\GitHub\\regex_coprocessor_safe\\proj\\regex_copro\\regex_copro.sim\\sim_1\\behav\\xsim\\test.vcd"
+	vcd_path = "test4_regex22_string1.vcd"
+	vcd_path = "test2x2_regex22_string1.vcd"
+	#vcd_path = "test_1.vcd"
+	#print('Give me a vcd file to parse')
+	#sys.exit(-1)
 
 vcd = VCDVCD(
-        vcd_path,
-        store_tvs=True,
-        store_scopes=True
-        )        
+		vcd_path,
+		store_tvs=True,
+		store_scopes=True
+		)        
 #for s in vcd.signals:
 #    print(s)
 
@@ -78,6 +79,8 @@ print(f'clock: {clk_period} {vcd.timescale["unit"]}')
 
 copro      = vcd[re.compile('.*a_regex_coprocessor$')]
 cur_state  = copro[re.compile('cur_state.*$')]
+cli		   = False
+full_screen= False
 #print(cur_state.tv)
 
 REGEX_COPRO_S_IDLE                          =0
@@ -104,90 +107,226 @@ engine_scopes = remove_non_maximal_scopes(engine_scopes)
 executed_instructions = []
 
 exe_per_engine = np.zeros((len(fetch_instant)-2, len(engine_scopes)))
-
+#list instruction loaded on the engines
 for cc_i, (start_cc, end_cc) in enumerate(start_end_cc):
-    print(f'Fetch {cc_i}')
-    for e_i,e in enumerate(engine_scopes):
-        
-        in_boundle = vcd[e]['in']
-        #print(in_boundle.subElements)
-        #print(in_boundle.name)
-        valid_data_ready_boundle = zip(in_boundle['valid'][start_cc:end_cc:clk_period], in_boundle['ready'][start_cc:end_cc:clk_period], in_boundle[re.compile('data.*')][start_cc:end_cc:clk_period])
-        valid_data_ready         = filter(lambda x: x[0]=='1' and x[1]=='1' and x[2] != 'x', valid_data_ready_boundle)
-        valid_data               = list(map(lambda x: x[2], valid_data_ready))
+	if cli:
+		print(f'Fetch {cc_i}')
+	for e_i,e in enumerate(engine_scopes):
+		
+		in_boundle = vcd[e]['in']
+		#print(in_boundle.subElements)
+		#print(in_boundle.name)
+		valid_data_ready_boundle = zip(in_boundle['valid'][start_cc:end_cc:clk_period], in_boundle['ready'][start_cc:end_cc:clk_period], in_boundle[re.compile('data.*')][start_cc:end_cc:clk_period])
+		valid_data_ready         = filter(lambda x: x[0]=='1' and x[1]=='1' and x[2] != 'x', valid_data_ready_boundle)
+		valid_data               = list(map(lambda x: x[2], valid_data_ready))
 
-        for d in valid_data:
-            #print(d)
-            value = int(d,2)
-            pc    = value //2
-            pc_is_directed_to_current = value %2
-            executed_instructions.append((pc_is_directed_to_current,pc))
-        exe_per_engine[cc_i, e_i] = len(valid_data)
-        print(f'\tEngine_{e_i} executed {len(valid_data)} instruction{ "s" if len(valid_data)> 1 else ""}')
+		for d in valid_data:
+			#print(d)
+			value = int(d,2)
+			pc    = value //2
+			pc_is_directed_to_current = value %2
+			executed_instructions.append((pc_is_directed_to_current,pc))
+		exe_per_engine[cc_i, e_i] = len(valid_data)
+		if cli:
+			print(f'\tEngine_{e_i} executed {len(valid_data)} instruction{ "s" if len(valid_data)> 1 else ""}')
+
+def press_handler_creator(draw_graph_i, num_graphs):
+	graph_number = 0
+	 
+	def press(event):
+		nonlocal graph_number
+		nonlocal num_graphs
+
+		#print('press', event.key)
+		sys.stdout.flush()
+		if event.key == 'right' or event.key == 'down' :
+			if(graph_number == num_graphs-1):
+				fig = plt.figure(0)
+				fig.text(0,0,"last available graph")
+				plt.draw()
+				plt.pause(0.001)
+			else:
+				graph_number += 1
+				#clear previous fig
+				fig = plt.figure(0)
+				fig.clf()
+				draw_graph_i(graph_number)
+				#set full screen
+				#fig.canvas.manager.full_screen_toggle()
+				#add event keypress
+				fig.canvas.mpl_connect('key_press_event', press)
+				fig.tight_layout()
+				plt.draw()
+				plt.pause(0.001)
+		elif (event.key == 'left' or event.key == 'up') :
+			if( graph_number == 0):
+				fig = plt.figure(0)
+				fig.text(0,0,"first available graph")
+				plt.draw()
+				plt.pause(0.001)
+			else: 
+				graph_number += -1
+				#clear previous fig
+				fig = plt.figure(0)
+				fig.clf()
+				draw_graph_i(graph_number)
+				#set full screen
+				#fig.canvas.manager.full_screen_toggle()
+				#add event keypress
+				#fig.canvas.mpl_connect('key_press_event', press)
+				fig.tight_layout()
+				plt.draw()
+				plt.pause(0.001)
+		elif event.key in [ 'ctrl+c','escape']:
+			fig = plt.figure(0)
+			plt.close(fig)
+			
+	return press
 
 def plot_engine_valid_in(engine_scopes, fetch_instant):
-    fig, axes = plt.subplots(max(len(engine_scopes),2), sharex=True, sharey=True)
-    for e_i,e in enumerate(engine_scopes):
-        #in
-        in_boundle = vcd[e]['in']
-        in_valid_ready_boundle = list(map(lambda x: x[0]=='1' and x[1]=='1' , zip(in_boundle['valid'][start_exe:end_exe:clk_period] , in_boundle['ready'][start_exe:end_exe:clk_period])))
-        #out
-        out_boundle = vcd[e]['out']
-        out_valid_ready_boundle = list(map(lambda x: x[0]=='1' and x[1]=='1', zip(out_boundle['valid'][start_exe:end_exe:clk_period], out_boundle['ready'][start_exe:end_exe:clk_period])))
-        
-        #plot valid&ready input signal
-        axes[e_i].axhline(0, color='.5')
-        plot_digital_signal(axes[e_i], np.arange(start_exe,end_exe,clk_period), in_valid_ready_boundle, label="in")
+	
+	data = {}
+	for e_i,e in enumerate(engine_scopes):
+		#in
+		in_boundle = vcd[e]['in']
+		in_validANDready_boundle = list(map(lambda x: x[0]=='1' and x[1]=='1' , zip(in_boundle['valid'][start_exe:end_exe:clk_period] , in_boundle['ready'][start_exe:end_exe:clk_period])))
+		#out
+		out_boundle = vcd[e]['out']
+		out_validANDready_signal = list(map(lambda x: x[0]=='1' and x[1]=='1', zip(out_boundle['valid'][start_exe:end_exe:clk_period], out_boundle['ready'][start_exe:end_exe:clk_period])))
+		
+		latency_signal 			 = list(map(lambda x: int(x,2),in_boundle[re.compile('latency.*')][start_exe:end_exe:clk_period]))
+		#plot valid&ready input signal
+		data[e] = {}
+		data[e]['in'        ] 	 = in_validANDready_boundle
+		data[e]['out'       ] 	 = out_validANDready_signal
+		data[e]['latency_in'] 	 = latency_signal
+		data[e]['fetch_instant'] = list(map(lambda x: (x-start_exe)//clk_period ,fetch_instant))
+	
 
-        offset = 1.5
-        axes[e_i].axhline(offset, color='.5')
-        plot_digital_signal(axes[e_i], np.arange(start_exe,end_exe,clk_period), offset+np.array(out_valid_ready_boundle),label='out', colour='blue')
+	n_elements_per_subPlot = 100
+	tot_number_of_subgraph = ((end_exe-start_exe)//clk_period)//n_elements_per_subPlot
+	
+	def plot_i_esim(i):
+		
+		nonlocal data
+		nonlocal n_elements_per_subPlot
+		offset = 1.5
+		fig = plt.figure(0)
+		fig.clf()
 
-        for f in fetch_instant:
-            axes[e_i].axvline(f, color='g')
-        
-        latency_signal = list(map(lambda x: int(x,2),in_boundle[re.compile('latency.*')][start_exe:end_exe:clk_period]))
-        axes[e_i].step(np.arange(start_exe,end_exe,clk_period), offset*2+np.array(latency_signal), label='latency_in')
-    
-        axes[e_i].set_ylabel(f'Engine_{e_i}')
-        
-    axes[0].legend()
-    plt.show()
+		start_exe = i*n_elements_per_subPlot
+		end_exe   = (i+1)*n_elements_per_subPlot
+		n_engines = len(data.keys())
+		for e_i,e in enumerate(data):
+			if e_i == 0:
+				ax = fig.add_subplot(n_engines,1,(e_i+1))
+			else:
+				ax = fig.add_subplot(n_engines,1,(e_i+1), sharex=ax, sharey=ax)
+
+			ax.axhline(0, color='.5')
+			plot_digital_signal(ax, np.arange(start_exe,end_exe,1), data[e]['in'     ][start_exe:end_exe], label="in")
+			
+			ax.axhline(offset, color='.5')
+			plot_digital_signal(ax, np.arange(start_exe,end_exe,1), offset+np.array(data[e]['out'    ][start_exe:end_exe]),label='out', colour='blue')
+
+			for f in data[e]['fetch_instant']:
+				if f >= start_exe and f < end_exe:
+					#print(f)
+					ax.axvline(f, color='g')
+			
+			ax.step(np.arange(start_exe,end_exe,1), offset*2+np.array(data[e]['latency_in'    ][start_exe:end_exe]), label=f'latency_in + {offset*2}')
+		
+			ax.set_ylabel(f'Engine_{e_i}')
+			
+				
+		fig.tight_layout()
+		plt.draw()
+		plt.legend( loc='upper left')
+		plt.pause(0.001)
+
+
+	fig = plt.figure(0)
+	fig.clf()
+	plot_i_esim(0)
+	press_handler = press_handler_creator(plot_i_esim,tot_number_of_subgraph)
+	#print(press_handler)
+	if(full_screen):
+		fig.canvas.manager.full_screen_toggle()
+	fig.canvas.mpl_connect('key_press_event', press_handler)
+	fig.tight_layout()
+	plt.draw()
+	plt.pause(0.001)
+	input("press a key to close")
 
 def plot_digital_signal(ax, x,y, clock_ticks=True, colour='r', label=''):
-    ax.axhline(0, color='.5')
-    if(clock_ticks):
-        for a_x in x:
-            ax.axvline(a_x, color='.5', linestyle='--', linewidth=0.5)
-    ax.step(x, y, colour, label=label)
-        
+	ax.axhline(0, color='.5')
+	if(clock_ticks):
+		for a_x in x:
+			ax.axvline(a_x, color='.5', linestyle='--', linewidth=0.5)
+	ax.step(x, y, colour, label=label)
+		
 plot_engine_valid_in(engine_scopes, fetch_instant)
 
-def plotHeatmap(heatmap, x_labels, y_labels ):
-    fig, ax = plt.subplots()
-    im = ax.imshow(heatmap)
-    ax.set_xticks(np.arange(len(x_labels)))
-    ax.set_yticks(np.arange(len(y_labels)))
-    ax.set_yticklabels(['ch_' +str(e) for e in range(len(y_labels))])
-    ax.set_xticklabels(['eng_'+str(e) for e in range(len(x_labels))])
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(y_labels)):
-        for j in range(len(x_labels)):
-            if heatmap[i, j] > 0:
-                ax.text(j, i,f'{heatmap[i, j]} ({int(100*heatmap[i, j]/sum(heatmap[i,:])) }%)',ha="center", va="center", color="w")
-    ax.set_title("Instructions executed by each Engine per each character")
-    fig.tight_layout()
-    plt.show()
+def plotLargeHeatmap(heatmap, x_labels, y_labels):
+	n_elements_per_subPlot = 8
+	tot_number_of_subgraph = len(y_labels)//n_elements_per_subPlot
+	def plot_i_esim(i):
+		subplotHeatmap(heatmap, x_labels, y_labels, i, n_elements_per_subPlot, reuse_fig=True, block=False)
 
-plotHeatmap(exe_per_engine,engine_scopes, start_end_cc)
+	fig = plt.figure(0)
+	fig.clf()
+	plot_i_esim(0)
+	press_handler = press_handler_creator(plot_i_esim,tot_number_of_subgraph)
+	#print(press_handler)
+	if(full_screen):
+		fig.canvas.manager.full_screen_toggle()
+	fig.canvas.mpl_connect('key_press_event', press_handler)
+	fig.tight_layout()
+	plt.draw()
+	plt.pause(0.001)
+	input("press a key to close")
+
+def subplotHeatmap(heatmap, x_labels, y_labels, i, n , reuse_fig=False, block=True):
+	start_y,end_y 	= i*n, (i+1)*n
+	sub_heatmap 	= heatmap[start_y:end_y,:]
+	y_sub_labels 	= y_labels[start_y:end_y]
+	plotHeatmap(sub_heatmap, x_labels,y_sub_labels, reuse_fig=reuse_fig, block=block)
+
+def plotHeatmap(heatmap, x_labels, y_labels, reuse_fig=False , block=True):
+	if reuse_fig:
+		fig = plt.figure(0)
+		fig.clf()
+		ax  = fig.add_subplot(1,1,1)
+	else:
+		fig, ax = plt.subplots()
+	im = ax.imshow(heatmap)
+	ax.set_xticks(np.arange(len(x_labels)))
+	ax.set_yticks(np.arange(len(y_labels)))
+	ax.set_yticklabels(y_labels)
+	ax.set_xticklabels(x_labels)
+	# Rotate the tick labels and set their alignment.
+	plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+		 rotation_mode="anchor")
+	# Loop over data dimensions and create text annotations.
+	for y in range(len(y_labels)):
+		for x in range(len(x_labels)):
+			if heatmap[y, x] > 0:
+				ax.text(x, y,f'{heatmap[y, x]} ({int(100*heatmap[y, x]/sum(heatmap[y,:])) }%)',ha="center", va="center")
+	ax.set_title("Instructions executed by each Engine per each character")
+	fig.tight_layout()
+	if block:
+		plt.show()
+	else:
+		plt.draw()
+		plt.pause(0.001)
+
+
+plotLargeHeatmap(exe_per_engine, [ f'Eng_{i}' for i,_ in enumerate(engine_scopes)], [ f'char_{i}' for i,_ in enumerate(start_end_cc)])
 
 BramScope = vcd[re.compile('.*a?(b|B)ram$')]
 #print(BramScope)
 valid_data_ready_boundle = BramScope['r_valid'][start_exe:end_exe:clk_period]
 valid_data_ready         = list(filter(lambda x: x[0]=='1', valid_data_ready_boundle))
-print(f'Memory utilization: {len(valid_data_ready)} / {duration_exe_cc} cc ({len(valid_data_ready)*clk_period/vcd.endtime}% )')
+print(f'Memory utilization: {len(valid_data_ready)} / {duration_exe_cc} cc ({int(100*len(valid_data_ready)*clk_period/vcd.endtime)}% )')
 
 print(f'~ CPI {duration_exe_cc//len(executed_instructions)} ')
