a7e98c8c4f02d32b1ec09c89d747e6c8ac01658e
DavideConficconi <davideconficconi@gmail.com>
2021-03-26 16:40:46 +0100

    [RES] minor not useful

--- extract.bash
+++ extract.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
-windows=(1)
+windows=(2)
 for j in ${windows[@]}
 do
 
-#for i in $(ls build_$j | grep u96_mesh_*)
-for i in 1
+for i in $(ls build_$j | grep u96_mesh_16)
+#for i in 16
 do
 	dsgn_name=u96_mesh_${i//u96_mesh_/}
 	prj_name=u96_mesh_${i//u96_mesh_/}
-	top_name=cicero_top_${i//u96_mesh_/}
+	top_name=cicero_top_mesh_${i//u96_mesh_/}
 	#echo "vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/u96_mesh_${i//u96_mesh_/} ${i//u96_mesh_/} $(pwd)/build_$j mesh"
-	#vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/u96_mesh_${i//u96_mesh_/} ${i//u96_mesh_/} $(pwd)/build_$j mesh
-	#vivado -mode batch -source extract.tcl -tclargs $(pwd)/build_$j/u96_mesh_${i//u96_mesh_/} ${i//u96_mesh_/} $(pwd)/build_$j mesh
+	vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/u96_mesh_${i//u96_mesh_/} ${i//u96_mesh_/} $(pwd)/build_$j mesh
+	vivado -mode batch -source extract.tcl -tclargs $(pwd)/build_$j/u96_mesh_${i//u96_mesh_/} ${i//u96_mesh_/} $(pwd)/build_$j mesh
 
 	cp $(pwd)/build_$j/$dsgn_name/$prj_name/${prj_name}.srcs/sources_1/bd/$top_name/hw_handoff/${top_name}.hwh $(pwd)/build_$j/cicero_top_mesh_${i//u96_mesh_/}_wrapper.hwh 
 
 done
-for i in $(ls build_$j | grep u96_ring_10)
+for i in $(ls build_$j | grep u96_ring_1)
 #for i in 10
 do
 
@@ -26,8 +26,8 @@ do
 	top_name=cicero_top_ring_${i//u96_ring_/}
 	echo " $dsgm_name    $prj_name     $top_name"
 	#echo "vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/u96_ring_${i//u96_ring_/} ${i//u96_ring_/} $(pwd)/build_$j ring"
-	#vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/u96_ring_${i//u96_ring_/} ${i//u96_ring_/} $(pwd)/build_$j ring
-	#vivado -mode batch -source extract.tcl -tclargs $(pwd)/build_$j/u96_ring_${i//u96_ring_/} ${i//u96_ring_/} $(pwd)/build_$j ring
+	vivado -mode batch -source extraction.tcl -tclargs $(pwd)/build_$j/${dsgn_name} ${i//u96_ring_/} $(pwd)/build_$j ring
+	vivado -mode batch -source extract.tcl -tclargs $(pwd)/build_$j/${dsgn_name} ${i//u96_ring_/} $(pwd)/build_$j ring
 	cp $(pwd)/build_$j/$dsgn_name/$prj_name/${prj_name}.srcs/sources_1/bd/$top_name/hw_handoff/${top_name}.hwh $(pwd)/build_$j/cicero_top_ring_${i//u96_ring_/}_wrapper.hwh 
 done
 done
