5edde279669f2b1df54e870e548a37194ca82ff8
davideconficconi <davideconficconi@gmail.com>
2021-04-03 10:44:33 +0000

    adding rank line for bitstreams

--- scripts/measurements/test_multi_bitstreans.sh
+++ scripts/measurements/test_multi_bitstreans.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 windows=(1)
-bit_fldr=/home/xilinx/test_cicero/
-benchmarks=(protomata clamAV brill snort)
+bit_fldr=/home/xilinx/cicero_scaling_tmp
+#benchmarks=(protomata clamAV brill)
+benchmarks=(brill)
+rank=4
 for bench in ${benchmarks[@]}
 do
 for i in ${windows[@]};
@@ -10,8 +12,8 @@ do
        	do 
 		for l in $(ls  ${bit_fldr}/queue_$i/$j/*.bit)
 	       	do 
-			python3 measure_rand.py -copro -bitstream $l -benchmark=${bench} -strfile="${bench}.input" -regfile="${bench}.regex" -format=pcre -skipException -window_value=queue_$i;
-			python3 measure_rand.py -copro -bitstream $l -benchmark=${bench} -strfile="${bench}.input" -regfile="${bench}.regex" -format=pythonre -skipException -window_value=queue_$i;
+#			python3 measure_rand.py -copro -bitstream $l -benchmark=${bench} -strfile="${bench}.input" -regfile="${bench}.regex" -format=pcre -skipException -window_value=queue_$i;
+			python3 measure_rand.py -copro -bitstream $l -benchmark=${bench}${rank} -strfile="${bench}.input" -regfile="${bench}${rank}.regex" -format=pcre -skipException -window_value=queue_$i;
 		done
        done
 done
