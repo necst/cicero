1e5f16ba4b1a869c59893dfc99e19bb269e50e6d
Andrea Somaini <andrea.somaini@mail.polimi.it>
2024-08-07 18:41:22 +0200

    Fix aggregation script (divide by clock freq to get time)

--- scripts/artifact_evaluation/benchmarks/aggregate_benchmarks_results.py
+++ scripts/artifact_evaluation/benchmarks/aggregate_benchmarks_results.py
@@ -15,6 +15,23 @@ sys.path.append(os.path.join(script_dir, '..', '..', '..',
                 'scripts', 'measurements', 'benchmark'))
 from aggregate import aggregate_result  # nopep8
 
+map_arcs_conf_to_clock_freq = {
+    'OLD 1x1': 150,
+    'OLD 1x4': 150,
+    'OLD 1x9': 150,
+    'OLD 1x16': 150,
+    'OLD 1x32': 150,
+    'NEW 8x1': 150,
+    'NEW 8x4': 150,
+    'NEW 8x9': 150,
+    'NEW 8x16': 150,
+    'NEW 16x1': 150,
+    'NEW 16x4': 150,
+    'NEW 16x9': 100,
+    'NEW 32x1': 150,
+    'NEW 32x4': 100,
+}
+
 COMPILER_NAME_OLD = 'OLD_PYT'
 COMPILER_NAME_NEW = 'NEW_CPP'
 
@@ -46,7 +63,7 @@ MICRO_BENCHMARKS_ARCS_CONFIGURATIONS = [
     'NEW 32x4',
 ]
 
-OLD_COMPILER_BENCHMARKS_REs_COUNT = -1
+OLD_COMPILER_BENCHMARKS_REs_COUNT = 10000
 OLD_COMPILER_BENCHMARKS_COMPILERS = [
     COMPILER_NAME_OLD,
     COMPILER_NAME_NEW,
@@ -56,7 +73,7 @@ OLD_COMPILER_BENCHMARKS_ARCS_CONFIGURATIONS = [
     'OLD 1x16',
 ]
 
-COMPLETE_BENCHMARKS_REs_COUNT = -1
+COMPLETE_BENCHMARKS_REs_COUNT = 10000
 COMPLETE_BENCHMARKS_COMPILERS = [
     COMPILER_NAME_NEW,
 ]
@@ -106,9 +123,10 @@ def aggregate_results_for_benchmarks_suite(map_arcs_conf_to_power, results_dir, 
                     results_dir, get_csv_name(arc_conf, benchmark, compiler_name, REs_count))
                 file_path, total_sum, avg_cc_re, total_timeout, total_fifo_full, other_errors, match_success_count, total_rows = aggregate_result(
                     benchmark_csv_path)
-                avg_energy_re = avg_cc_re * map_arcs_conf_to_power[arc_conf]
+                avg_time_re = avg_cc_re / map_arcs_conf_to_clock_freq[arc_conf]
+                avg_energy_re = avg_time_re * map_arcs_conf_to_power[arc_conf]
                 table.add_row(benchmark, arc_conf, compiler_name,
-                              str(avg_cc_re), str(avg_energy_re))
+                              str(avg_time_re), str(avg_energy_re))
 
     console = Console()
     console.print(table)
@@ -132,7 +150,8 @@ def aggregate_print_table_for_microbench(map_arcs_conf_to_power, results_dir):
                 results_dir, get_csv_name(arc_conf, benchmark, MICRO_BENCHMARKS_COMPILERS[0], MICRO_BENCHMARKS_REs_COUNT))
             file_path, total_sum, avg_cc_re, total_timeout, total_fifo_full, other_errors, match_success_count, total_rows = aggregate_result(
                 benchmark_csv_path)
-            avg_energy_re = avg_cc_re * map_arcs_conf_to_power[arc_conf]
+            avg_time_re = avg_cc_re / map_arcs_conf_to_clock_freq[arc_conf]
+            avg_energy_re = avg_time_re * map_arcs_conf_to_power[arc_conf]
             new_row.append(str(avg_energy_re))
 
             cur_row_total_energy += avg_energy_re
@@ -147,17 +166,20 @@ def aggregate_print_table_for_microbench(map_arcs_conf_to_power, results_dir):
 def aggregate_print_table_for_compilers_compare(results_dir):
     table = Table(
         title='[Fig. 11] Compilers impact on the old architecture with 9 and 16 cores considering the average execution times per RE for each benchmark.')
-    table.add_column("Benchmark", style="bold")
+    table.add_column("Compiler", style="bold")
+    table.add_column("Benchmark", style="purple")
     table.add_column('Arc Conf', style="cyan")
     table.add_column("AVG µs/RE", style="magenta")
 
     for benchmark in BENCHMARKS:
         for arc_conf in OLD_COMPILER_BENCHMARKS_ARCS_CONFIGURATIONS:
-            benchmark_csv_path = os.path.join(
-                results_dir, get_csv_name(arc_conf, benchmark, OLD_COMPILER_BENCHMARKS_COMPILERS[0], OLD_COMPILER_BENCHMARKS_REs_COUNT))
-            file_path, total_sum, avg_cc_re, total_timeout, total_fifo_full, other_errors, match_success_count, total_rows = aggregate_result(
-                benchmark_csv_path)
-            table.add_row(benchmark, arc_conf, str(avg_cc_re))
+            for compiler in OLD_COMPILER_BENCHMARKS_COMPILERS:
+                benchmark_csv_path = os.path.join(
+                    results_dir, get_csv_name(arc_conf, benchmark, compiler, OLD_COMPILER_BENCHMARKS_REs_COUNT))
+                file_path, total_sum, avg_cc_re, total_timeout, total_fifo_full, other_errors, match_success_count, total_rows = aggregate_result(
+                    benchmark_csv_path)
+                avg_time_re = avg_cc_re / map_arcs_conf_to_clock_freq[arc_conf]
+                table.add_row(compiler, benchmark, arc_conf, str(avg_time_re))
 
     console = Console()
     console.print(table)
@@ -185,15 +207,17 @@ def aggregate_print_table_for_complete_bench(map_arcs_conf_to_power, results_dir
                 results_dir, get_csv_name(arc_conf, benchmark, COMPLETE_BENCHMARKS_COMPILERS[0], COMPLETE_BENCHMARKS_REs_COUNT))
             file_path, total_sum, avg_cc_re, total_timeout, total_fifo_full, other_errors, match_success_count, total_rows = aggregate_result(
                 benchmark_csv_path)
+            avg_time_re = avg_cc_re / map_arcs_conf_to_clock_freq[arc_conf]
 
             if os.path.basename(file_path).startswith('OLD 1x9'):
-                baseline_time = avg_cc_re
-                baseline_energy = avg_cc_re * map_arcs_conf_to_power[arc_conf]
+                baseline_time = avg_time_re
+                baseline_energy = avg_time_re * \
+                    map_arcs_conf_to_power[arc_conf]
 
-            time_speedup = avg_cc_re / baseline_time
+            time_speedup = avg_time_re / baseline_time
             table14.add_row(benchmark, arc_conf, str(time_speedup))
 
-            avg_energy_re = avg_cc_re * map_arcs_conf_to_power[arc_conf]
+            avg_energy_re = avg_time_re * map_arcs_conf_to_power[arc_conf]
             energy_speedup = baseline_energy / avg_energy_re
             table15.add_row(benchmark, arc_conf, str(energy_speedup))
 
